3f2b75729e3e0525c0552aefe8447586f1f4d873
Volodymyr Beimuk <tetrenkuchioan@gmail.com>
2024-10-17 06:05:32 +0300

    advanced errs done

--- rustlings/exercises/advanced_errors/advanced_errs2.rs
+++ rustlings/exercises/advanced_errors/advanced_errs2.rs
@@ -16,7 +16,7 @@
 // 4. Complete the partial implementation of `Display` for
 //    `ParseClimateError`.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::error::Error;
 use std::fmt::{self, Display, Formatter};
@@ -47,6 +47,7 @@ impl From<ParseIntError> for ParseClimateError {
 impl From<ParseFloatError> for ParseClimateError {
     fn from(e: ParseFloatError) -> Self {
         // TODO: Complete this function
+        Self::ParseFloat(e)
     }
 }
 
@@ -64,6 +65,19 @@ impl Display for ParseClimateError {
         match self {
             NoCity => write!(f, "no city name"),
             ParseFloat(e) => write!(f, "error parsing temperature: {}", e),
+            ParseInt(e) => write!(f, "error parsing year: {}", e),
+            Empty => write!(f, "empty input"),
+            BadLen => write!(f, "incorrect number of fields"),
+        }
+    }
+}
+
+impl std::error::Error for ParseClimateError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ParseClimateError::ParseInt(e) => Some(e),
+            ParseClimateError::ParseFloat(e) => Some(e),
+            _ => None,
         }
     }
 }
@@ -88,8 +102,16 @@ impl FromStr for Climate {
     // TODO: Complete this function by making it handle the missing error
     // cases.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParseClimateError::Empty);
+        }
+        
         let v: Vec<_> = s.split(',').collect();
+        if v.len() != 3 {
+            return Err(ParseClimateError::BadLen);
+        }
         let (city, year, temp) = match &v[..] {
+            ["", year, temp] => return Err(ParseClimateError::NoCity),
             [city, year, temp] => (city.to_string(), year, temp),
             _ => return Err(ParseClimateError::BadLen),
         };
